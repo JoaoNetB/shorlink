1ba27a7026ffd0224fc06d22314d6b1ae1acc59d
João Bevolo <netojb156@gmail.com>
2025-04-15 14:59:36 -0300

    feat(home view): add banner section
    - Add banner section to home view
    - Add illustrations for banners

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,4 +12,22 @@
             <button class="border-2 rounded-lg text-sm border-primary bg-primary  text-white pt-2.5 pb-2.5 pl-2.5 pr-2.5 hover:scale-105">login</button>
         </div>
     </nav>
+    <div class="flex justify-around items-center mt-20 mb-40">
+        <div class="max-w-2xl">
+            <h1 class="text-5xl font-bold">Shorten, share, succeed!</h1>
+            <h2 class="font-normal text-3xl max-w-xl mt-2">Use friendly links to connect your audience</h2>
+        </div>
+        <figure>
+            <img class="min-w-sm" src="/images/undraw_link_shortener.svg" alt="Illustration of a character holding a screen displaying a link symbol." />
+        </figure>
+    </div>
+    <div class="flex justify-around items-center mt-20 mb-40">
+        <figure>
+            <img class="min-w-sm" src="/images/undraw_charts.svg" alt="Illustration of a character analyzing a screen with charts and data graphs." />
+        </figure>
+        <div class="max-w-2xl">
+            <h1 class="text-5xl font-bold">See who cares about your content!</h1>
+            <h2 class="font-normal text-3xl max-w-xl mt-2">Understand your visitors in real-time</h2>
+        </div>
+    </div>
 @endsection
